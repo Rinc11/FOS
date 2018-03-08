32ee5abb3365a556aa18bf34efa99d875357976f
suttema2 <suttema2@students.zhaw.ch>
2018-03-08 14:25:52 +0100

    SQL-Script ins Projekt eingebunden

--- source/fos/dataBaseUpdater/src/main/resources/dataBaseUpdateSkript.sql
+++ source/fos/dataBaseUpdater/src/main/resources/dataBaseUpdateSkript.sql
@@ -1,13 +1,83 @@
 --#1:
-CREATE TABLE bla (
-  col int not null
+CREATE TABLE "Trip"
+(
+  "VehicleID"  INTEGER NOT NULL,
+  "TripID"     INTEGER NOT NULL,
+  "StartTime"  TIMESTAMP,
+  "EndTime"    TIMESTAMP,
+  "PlaceStart" VARCHAR,
+  "PlaceEnd"   VARCHAR,
+  "Start_km"   INTEGER,
+  "End_km"     INTEGER,
+  "Type"       VARCHAR,
+  "Username"   VARCHAR NOT NULL,
+  CONSTRAINT "PK_TRIP"
+  PRIMARY KEY ("VehicleID", "TripID", "Username")
 );
 
-CREATE TABLE blaa (
-  col int not null
+CREATE TABLE "Vehicles"
+(
+  "VehicleID"    INTEGER NOT NULL
+    CONSTRAINT "PK_VEHICLES"
+    PRIMARY KEY,
+  "Serialnumber" INTEGER,
+  "Brand"        VARCHAR,
+  "Type"         VARCHAR,
+  "BuildYear"    INTEGER,
+  "FuelType"     VARCHAR
 );
 
---#2:
-CREATE TABLE blx (
-  col int not null
+ALTER TABLE "Trip"
+  ADD CONSTRAINT "FK_TRIP_VEHICLES"
+FOREIGN KEY ("VehicleID") REFERENCES "Vehicles";
+
+CREATE TABLE "Refueling"
+(
+  "RefuelID"  INTEGER NOT NULL,
+  "VehicleID" INTEGER NOT NULL
+    CONSTRAINT "FK_Refueling_VEHICLEID"
+    REFERENCES "Vehicles",
+  "Date"      DATE,
+  "FuelType"  VARCHAR,
+  "Price"     DOUBLE PRECISION,
+  "Amount"    DOUBLE PRECISION,
+  CONSTRAINT "PK_Refueling"
+  PRIMARY KEY ("RefuelID", "VehicleID")
 );
+
+CREATE TABLE "Person"
+(
+  "Username"     VARCHAR NOT NULL
+    CONSTRAINT "PK_PERSON"
+    PRIMARY KEY,
+  "Firstname"    VARCHAR,
+  "Lastname"     VARCHAR,
+  "AHV"          VARCHAR,
+  "Street"       VARCHAR,
+  "Place"        VARCHAR,
+  "Email"        VARCHAR,
+  "Password"     VARCHAR,
+  "PasswordHint" VARCHAR,
+  "Locked_YN"    BOOLEAN,
+  "LoginTry"     INTEGER,
+  "Usertype"     VARCHAR,
+  "Deleted_YN"   BOOLEAN
+);
+
+ALTER TABLE "Trip"
+  ADD CONSTRAINT "FK_TRIP_PERSON"
+FOREIGN KEY ("Username") REFERENCES "Person";
+
+CREATE TABLE refuels
+(
+  "VehicleID" INTEGER,
+  "Username"  VARCHAR
+    CONSTRAINT "FK_REFUELS_PERSON_USERNAME"
+    REFERENCES "Person",
+  "RefuelID"  INTEGER,
+  CONSTRAINT "UC_REFUELS"
+  UNIQUE ("VehicleID", "Username", "RefuelID"),
+  CONSTRAINT "FK_REFUELS_VEHICLEID_REFUELID"
+  FOREIGN KEY ("VehicleID", "RefuelID") REFERENCES "Refueling" ("VehicleID", "RefuelID")
+);
+
