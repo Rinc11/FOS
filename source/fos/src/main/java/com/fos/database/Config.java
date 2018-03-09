f529169040e75df724f5ab68201fdf05db12f3c4
Reto Mayer <mayerret@students.zhaw.ch>
2018-03-09 20:44:56 +0100

    javadoc verfollständigen

--- source/fos/src/main/java/com/fos/database/Config.java
+++ source/fos/src/main/java/com/fos/database/Config.java
@@ -7,12 +7,21 @@ import java.sql.Statement;
 import java.util.ArrayList;
 import java.util.List;
 
+/**
+ * stellt ein fosConfig Datensatz dar
+ */
 public class Config {
     private Config(String value, String id){
         this.id = id;
         this.value = value;
     }
 
+    /**
+     * gibt alle Konfig Einträge zurück
+     * @param conn Die Connection zur Datenbank
+     * @return Eine Liste von Konfig einträge
+     * @throws SQLException bei Datenbankfehler wird diese Exeption geworfen
+     */
     public static List<Config> getAllConfig(Connection conn) throws SQLException {
         List<Config> result = new ArrayList<>();
         Statement statement = conn.createStatement();
@@ -26,11 +35,19 @@ public class Config {
     private String id;
     private String value;
 
+    /**
+     * gibt den Schlüssel des Konfig Eintrages zurück
+     * @return Schlüssel bei der Konfig Tabelle
+     */
     public String getId() {
         return id;
     }
+
+    /**
+     * gibt den Inhalt des Konfig Eintrages zurück
+     * @return Wert des Konfig Eintrages
+     */
     public String getValue() {
         return value;
     }
-
 }
--- source/fos/src/main/java/com/fos/login/Login.java
+++ source/fos/src/main/java/com/fos/login/Login.java
@@ -8,8 +8,19 @@ import javax.servlet.http.HttpServletResponse;
 import javax.servlet.http.HttpSession;
 import java.io.IOException;
 
+/***
+ * Login servlet; empfängt das Anmelde Formular und überprüft den Login
+ */
 @WebServlet(name = "/Login", value = "/Login")
 public class Login extends HttpServlet {
+
+    /**
+     * Empfängt den Post Requst des Anmelde Formulares
+     * @param request Daten des Anmelde Formulares
+     * @param response
+     * @throws ServletException
+     * @throws IOException
+     */
     protected void doPost(HttpServletRequest request, HttpServletResponse response) throws ServletException, IOException {
         String userName = request.getParameter("userName");
         String pass = request.getParameter("pass");
