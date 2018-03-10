26e30ba2d8f49d8c8ed251352682c1dc9439fb62
Reto Mayer <mayerret@students.zhaw.ch>
2018-03-10 19:27:15 +0100

    Datanbank update(datenbank muss vorher manuell gelöscht werden(ganzes Schema))

--- source/fos/dataBaseUpdater/src/main/resources/dataBaseUpdateSkript.sql
+++ source/fos/dataBaseUpdater/src/main/resources/dataBaseUpdateSkript.sql
@@ -3,11 +3,10 @@ CREATE TYPE ENUM_TRIP_TYPE AS ENUM ('privat', 'geschäftlich');
 CREATE TYPE ENUM_VEHICLES_FUELTYPE AS ENUM ('Benzin', 'Diesel', 'Strom', 'Erdgas');
 CREATE TYPE ENUM_PERSON_USERTYPE AS ENUM ('Admin', 'Mitarbeiter');
 
-
 CREATE TABLE "Trip"
 (
-  "TripID"     INTEGER NOT NULL,
-  "VehicleID"  INTEGER NOT NULL,
+  "TripID"     SERIAL,
+  "VehicleID"  SERIAL NOT NULL,
   "StartTime"  TIMESTAMP NOT NULL,
   "EndTime"    TIMESTAMP,
   "PlaceStart" VARCHAR NOT NULL,
@@ -22,10 +21,10 @@ CREATE TABLE "Trip"
 
 CREATE TABLE "Vehicles"
 (
-  "VehicleID"    INTEGER NOT NULL
+  "VehicleID"    SERIAL
     CONSTRAINT "PK_VEHICLES"
     PRIMARY KEY,
-  "Serialnumber" INTEGER NOT NULL,
+  "Serialnumber" VARCHAR NOT NULL,
   "Brand"        VARCHAR DEFAULT '',
   "Type"         VARCHAR DEFAULT '',
   "BuildYear"    INTEGER NOT NULL,
@@ -45,15 +44,15 @@ CREATE TABLE "Person"
     PRIMARY KEY,
   "Firstname"    VARCHAR NOT NULL,
   "Lastname"     VARCHAR NOT NULL,
-  "AHV"          VARCHAR,
-  "Street"       VARCHAR,
-  "Place"        VARCHAR,
-  "Email"        VARCHAR,
+  "AHV"          VARCHAR DEFAULT '',
+  "Street"       VARCHAR DEFAULT '',
+  "Place"        VARCHAR DEFAULT '',
+  "Email"        VARCHAR DEFAULT '',
   "Password"     VARCHAR NOT NULL,
   "PasswordHint" VARCHAR DEFAULT '',
   "Locked_YN"    BOOLEAN DEFAULT FALSE,
   "LoginTry"     INTEGER DEFAULT 0,
-  "Usertype"     ENUM_PERSON_USERTYPE NOT NULL,
+  "Usertype"     ENUM_PERSON_USERTYPE DEFAULT 'Mitarbeiter',
   "Deleted_YN"   BOOLEAN DEFAULT FALSE
 );
 
@@ -61,35 +60,24 @@ ALTER TABLE "Trip"
   ADD CONSTRAINT "FK_TRIP_PERSON"
 FOREIGN KEY ("Username") REFERENCES "Person";
 
-
+--#2:
 -- INSERT PERSON
 INSERT INTO fos."Person" ("Username", "Firstname", "Lastname", "AHV", "Street", "Place", "Email", "Password", "PasswordHint", "Locked_YN", "LoginTry", "Usertype", "Deleted_YN") VALUES ('suttema2', 'Marco', 'Sutter', '756.1234.5678.90', 'Feldstrasse 1', 'Amriswil', 'suttema2@students.zhaw.ch', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '1234', false, 0, 'Admin', false);
 INSERT INTO fos."Person" ("Username", "Firstname", "Lastname", "AHV", "Street", "Place", "Email", "Password", "PasswordHint", "Locked_YN", "LoginTry", "Usertype", "Deleted_YN") VALUES ('wipffab', 'Fabian', 'Wipf', '756.1234.5678.90', 'Feldstrasse 1', 'Frauenfeld', 'wipffab@students.zhaw.ch', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '1234', false, 0, 'Admin', false);
 INSERT INTO fos."Person" ("Username", "Firstname", "Lastname", "AHV", "Street", "Place", "Email", "Password", "PasswordHint", "Locked_YN", "LoginTry", "Usertype", "Deleted_YN") VALUES ('ruegjon', 'Jonas', 'Rüegge', '756.1234.5678.90', 'Feldstrasse 1', 'Romanshorn', 'ruegjon@students.zhaw.ch', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '1234', false, 0, 'Admin', false);
-INSERT INTO fos."Person" ("Username", "Firstname", "Lastname", "AHV", "Street", "Place", "Email", "Password", "PasswordHint", "Locked_YN", "LoginTry", "Usertype", "Deleted_YN") VALUES ('mayeret', 'Reto', 'Mayer', '756.1234.5678.90', 'Feldstrasse 1', 'Berg', 'mayeret@students.zhaw.ch', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '1234', false, 0, 'Admin', false);
+INSERT INTO fos."Person" ("Username", "Firstname", "Lastname", "AHV", "Street", "Place", "Email", "Password", "PasswordHint", "Locked_YN", "LoginTry", "Usertype", "Deleted_YN") VALUES ('mayerret', 'Reto', 'Mayer', '756.1234.5678.90', 'Feldstrasse 1', 'Berg', 'mayeret@students.zhaw.ch', '03ac674216f3e15c761ee1a5e255f067953623c8b388b4459e13f978d7c846f4', '1234', false, 0, 'Admin', false);
 
 --INSERT VEHICLES
 
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1000, 1234-312-32, 'Ford', 'Focus', 2010, 'Benzin');
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1001, 1234-12-43, 'Fiat', 'Panda', 2011, 'Benzin');
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1002, 1234-234-21, 'VW', 'Golf', 2014, 'Benzin');
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1003, 1234-123-23, 'BMW', 'X3', 2006, 'Diesel');
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1004, 1234-123-65, 'Ford', 'Fiesta', 2004, 'Erdgas');
-INSERT INTO fos."Vehicles" ("VehicleID", "Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1005, 1234-123-54, 'OPEL', 'Astra', 2014, 'Benzin');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-312-32, 'Ford', 'Focus', 2010, 'Benzin');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-12-43, 'Fiat', 'Panda', 2011, 'Benzin');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-234-21, 'VW', 'Golf', 2014, 'Benzin');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-123-23, 'BMW', 'X3', 2006, 'Diesel');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-123-65, 'Ford', 'Fiesta', 2004, 'Erdgas');
+INSERT INTO fos."Vehicles" ("Serialnumber", "Brand", "Type", "BuildYear", "FuelType") VALUES (1234-123-54, 'OPEL', 'Astra', 2014, 'Benzin');
 
 -- INSERT TRIP
 
-INSERT INTO fos."Trip" ("VehicleID", "TripID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (1000, 10000, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'privat', 'suttema2');
-INSERT INTO fos."Trip" ("VehicleID", "TripID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (1001, 10001, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'geschäftlich', 'wipffab');
-INSERT INTO fos."Trip" ("VehicleID", "TripID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (1004, 10002, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'privat', 'mayeret');
-
---#2:
-ALTER TABLE fos."Trip" DROP CONSTRAINT "FK_TRIP_PERSON";
-UPDATE fos."Person" SET "Username" = 'mayerret' WHERE "Username" = 'mayeret';
-UPDATE fos."Trip" SET "Username" = 'mayerret' WHERE "Username" = 'mayeret';
-
-ALTER TABLE fos."Trip"
-  ADD CONSTRAINT "FK_TRIP_PERSON"
-FOREIGN KEY ("Username") REFERENCES fos."Person";
-
-
+INSERT INTO fos."Trip" ("VehicleID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (1, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'privat', 'suttema2');
+INSERT INTO fos."Trip" ("VehicleID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (2, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'geschäftlich', 'wipffab');
+INSERT INTO fos."Trip" ("VehicleID", "StartTime", "EndTime", "PlaceStart", "PlaceEnd", "Start_km", "End_km", "Type", "Username") VALUES (3, '2018-03-08 13:04:03.614000', '2018-03-09 13:04:10.340000', 'Frauenfeld', 'Winterthur', 100, 130, 'privat', 'mayerret');
