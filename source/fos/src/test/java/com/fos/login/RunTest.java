ecacd55fb8bf1d76c18b6bdcc02a0fd05b83bbc0
Reto Mayer <mayerret@students.zhaw.ch>
2018-03-13 21:46:45 +0100

    Eigener Benutezre für die Tests erstellt

--- source/fos/src/test/java/com/fos/login/RunTest.java
+++ source/fos/src/test/java/com/fos/login/RunTest.java
@@ -4,7 +4,6 @@ import com.fos.database.NotLoadedExeption;
 import com.fos.database.Person;
 import org.junit.Test;
 import org.mockito.ArgumentCaptor;
-import org.mockito.Mock;
 
 import javax.servlet.http.HttpServletRequest;
 import javax.servlet.http.HttpServletResponse;
@@ -13,8 +12,13 @@ import javax.servlet.http.HttpSession;
 import java.io.IOException;
 import java.sql.SQLException;
 
-import static org.junit.Assert.*;
-import static org.mockito.Mockito.*;
+import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertTrue;
+import static org.mockito.ArgumentMatchers.eq;
+import static org.mockito.Mockito.mock;
+import static org.mockito.Mockito.verify;
+import static org.mockito.Mockito.when;
+
 
 public class RunTest {
 
@@ -37,19 +41,17 @@ public class RunTest {
     @Test
     public void testLogin() throws IOException, NotLoadedExeption {
         HttpServletRequest request = mock(HttpServletRequest.class);
-        when(request.getParameter("userName")).thenReturn("mayerret");
+        when(request.getParameter("userName")).thenReturn("testUser");
         when(request.getParameter("pass")).thenReturn("1234");
 
         HttpSession session = mock(HttpSession.class);
         when(request.getSession()).thenReturn(session);
 
         HttpServletResponse response = mock(HttpServletResponse.class);
-
-
         Login lo = new Login(request, response);
         ArgumentCaptor<Person> argument = ArgumentCaptor.forClass(Person.class);
-        verify(session).setAttribute(eq ("userLoggedIn"), argument.capture());
-        assertEquals("mayerret", argument.getValue().getUserName());
+        verify(session).setAttribute(eq("userLoggedIn"), argument.capture());
+        assertEquals("testUser", argument.getValue().getUserName());
         verify(response).sendRedirect("home.jsp");
     }
 }
