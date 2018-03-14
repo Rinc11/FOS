83ca97408e0d8777444d594262cf1155bebe5ff0
Jonas Rüegge <rueegjon@students.zhaw.ch>
2018-03-14 22:29:59 +0100

    IntegrationsTest und SystemTest Entwurf ohne Funktionen

--- source/fos/src/test/java/IntegrationTest.java
+++ source/fos/src/test/java/IntegrationTest.java
@@ -2,4 +2,53 @@
  * Tests zur Prüfung der Module und deren Zusammenspiel
  */
 public class IntegrationTest {
+
+    /**
+     * Loginseite
+     * * Anmeldung
+     * * Abmeldung
+     */
+
+    /**
+     * Benutzer erstellen
+     * Benutzer anpassen
+     * Benutzer löschen / sperren
+     */
+    /**
+     * Passwort ändern
+     */
+
+    /**
+     * Benutzerverwaltungsseite
+     */
+
+    /**
+     * Übersicht aller Fahrzeuge
+     */
+    /**
+     * Fahrzeug ertellen
+     */
+    /**
+     * Fahrzeug bearbeiten
+     */
+    /**
+     * Fahrzeug löschen
+     */
+
+    /**
+     * Start erfassen
+     */
+    /**
+     * Stop erfassen
+     */
+
+    /**
+     * Benutzerspezifische Startseite
+     */
+    /**
+     * Gesamtauswertung
+     */
+    /**
+     * Export als CSV
+     */
 }
