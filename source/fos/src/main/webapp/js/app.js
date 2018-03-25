c606c46752e24f3d2d04b9229ff0487b03ac96cd
suttema2 <suttema2@students.zhaw.ch>
2018-03-25 23:58:00 +0200

    Show-Button für das Passwortfeld implementiert

--- source/fos/src/main/webapp/js/app.js
+++ source/fos/src/main/webapp/js/app.js
@@ -43,3 +43,25 @@ function post(path, params) {
     document.body.appendChild(form);
     form.submit();
 }
+$(document).ready(function()
+{
+    $("#showhide").click(function()
+    {
+        if ($(this).data('val') == "1")
+        {
+            $("#pwd").prop('type','text');
+            $("#eye").attr("class","glyphicon glyphicon-eye-close");
+            $(this).data('val','0');
+        }
+        else
+        {
+            $("#pwd").prop('type', 'password');
+            $("#eye").attr("class","glyphicon glyphicon-eye-open");
+            $(this).data('val','1');
+        }
+    });
+});
+
+
+
+
