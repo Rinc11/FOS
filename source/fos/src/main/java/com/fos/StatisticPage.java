5f766166bbfb105d40d342018140c31a0eeab174
mayerret <mayer.reto@gmx.ch>
2018-04-21 09:37:36 +0200

    Auswertung Filter jsp logik

--- source/fos/src/main/java/com/fos/StatisticPage.java
+++ source/fos/src/main/java/com/fos/StatisticPage.java
@@ -1,15 +1,22 @@
 package com.fos;
 
 import com.fos.database.Config;
+import com.fos.database.Person;
+import com.fos.database.Trip;
+import com.fos.database.Vehicle;
 import com.fos.tools.FosPage;
+import com.fos.tools.Helper;
+import com.fos.tools.Logging;
 
 import javax.servlet.http.HttpServletRequest;
+import java.sql.Connection;
+import java.sql.SQLException;
 import java.util.ArrayList;
 import java.util.List;
 
 public class StatisticPage extends FosPage {
     private String jspFile = "/WEB-INF/jsp/statistic.jsp";
-
+    private List<Trip> filteredTrips;
     /**
      * Erstellt eine neue Statistic Seite
      *
@@ -17,6 +24,7 @@ public class StatisticPage extends FosPage {
      */
     public StatisticPage(HttpServletRequest request) {
         super(request, false);
+        //filterdTrips = bla
     }
 
     @Override
@@ -28,5 +36,40 @@ public class StatisticPage extends FosPage {
 
         List<Config> result = new ArrayList<>();
         return result;
+    }
+
+
+    public List<Vehicle> getVehiclesToChoose() {
+        Connection conn = null;
+        try {
+            conn = Helper.getConnection();
+            return Vehicle.getAllVehicles(conn);
+        } catch (SQLException e) {
+            Logging.logDatabaseException(request, e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                Logging.logConnectionNotCloseable(e);
+            }
+        }
+        return new ArrayList<>();
+    }
+
+    public List<Person> getPersonToChoose(){
+        Connection conn = null;
+        try {
+            conn = Helper.getConnection();
+            return Person.getAllPersons(conn);
+        } catch (SQLException e) {
+            Logging.logDatabaseException(request, e);
+        } finally {
+            try {
+                conn.close();
+            } catch (SQLException e) {
+                Logging.logConnectionNotCloseable(e);
+            }
+        }
+        return new ArrayList<>();
     }
 }
